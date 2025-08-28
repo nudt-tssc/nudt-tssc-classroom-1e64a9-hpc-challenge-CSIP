d9341b4ba551553529847745ea250833377465b1
wingrew <114449728+wingrew@users.noreply.github.com>
2025-08-28 22:23:58 +0800

    Update gpu.cu

--- gpu.cu
+++ gpu.cu
@@ -3,15 +3,6 @@
 请补充具体实现,于gpu_gemm_opt中调用gpu_gemm_opt_kernel，布局自定
 */
 
-#define OFFSET(row, col, ld) ((row) * (ld) + (col))
-#define FLOAT4(pointer) (reinterpret_cast<float4*>(&(pointer))[0])
-    
-const int BM = 128;
-const int BN = 128;
-const int BK = 8;
-const int TM = 8;
-const int TN = 8;
-
 
 __global__ void gpu_gemm_opt_kernel(
     float * __restrict__ a,
